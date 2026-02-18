d88f9089a852294f9dc9da37a31faa6b292997fb
Gagan S <gaganshetty1996@gmail.com>
2026-02-18 10:27:57 +0530

    updated1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";  // 👈 import HashRouter
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
